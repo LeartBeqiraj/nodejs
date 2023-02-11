20e736ec5de5de669ea53fb50eb8a3ba393ba384
Leart Beqiraj <leartbeqiraj@kodelabs.com>
2023-02-11 23:56:49 +0100

    commit x

--- app/app.js
+++ app/app.js
@@ -6,12 +6,17 @@ function traffic(path) {
 }
 
 app.get("/", (req, res) => {
-  res.send("hi mom");
+  res.send(`
+  <h1>Hi mom!</h1>
+  <a href="/whoami">Who am I?</a>
+  `);
   traffic(req.url);
 });
 
 app.get("/whoami", (req, res) => {
-  res.send("I am Full Stack DevOps Engineer");
+  res.send(`
+  <h1>Leeart mreti ---> Full Stack DevOps Engineer</h1>
+  `);
   traffic(req.url);
 });
 
